4e3a4c37783f22e9328ee1353f8e9d638b40c1b5
Paul Marquardt <marqrdt@gmail.com>
2025-01-01 19:36:50 -0700

    Added SIG* handlers. Fixed timing issues.

--- src/python/main.py
+++ src/python/main.py
@@ -4,6 +4,8 @@ import asyncio
 import signal
 import json
 import random
+import logging
+import sys
 
 class GoodThoughtNotifier:
     def __init__(self, message_file=None):
@@ -19,28 +21,38 @@ class GoodThoughtNotifier:
         self.app_config = self.messages_json.get('app_config')
         self.app_name = self.app_config["app_name"]
 
-    async def run(self):
+    def get_random_message(self):
         messages_length = len(self.messages_json.get('messages'))
+        return self.messages_json.get('messages')[ self.rand.randint(0, messages_length-1) ]['text']
 
+    async def run(self):
+        stop_event = asyncio.Event()
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal.SIGINT, self.stop_app)
+        loop.add_signal_handler(signal.SIGTERM, self.stop_app)
         while True:
-            message = self.messages_json.get('messages')[ self.rand.randint(0, messages_length-1) ]
-            #dtn = self.notifier( message['text'])
+            message = self.get_random_message()
+            timeout_time = self.rand.randint(self.app_config["min_message_interval"], self.app_config["max_message_interval"])
             #print(f"notification id is {dtn.id}, summary is {dtn.summary}")
             #dtn.set_id(dtn.id).set_timeout(app_config["min_message_interval"])
-            await self.notifier.send(
+            await asyncio.shield( self.notifier.send(
                 title=self.app_name,
-                message=message['text'],
-                timeout=int(self.app_config["min_message_interval"] / 2)
-
-            )
-            stop_event = asyncio.Event()
-            loop = asyncio.get_running_loop()
-            loop.add_signal_handler(signal.SIGINT, stop_event.set)
-            loop.add_signal_handler(signal.SIGTERM, stop_event.set)
-            await asyncio.sleep(self.rand.randint(self.app_config["min_message_interval"], self.app_config["max_message_interval"])) 
+                message=message,
+                timeout=int(timeout_time / 2)
+            ))
+            await asyncio.sleep(timeout_time) 
             await self.notifier.clear_all()
+    def stop_app(self):
+        self.notifier.send(
+                title="One last thing...",
+                message=self.get_random_message(),
+                timeout=int(self.app_config["min_message_interval"] / 2)
+            )
+        print(f"Stopping application {self.app_config['app_name']}")
+        sys.exit(0)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     message_file = "messages.json"
     gtn = GoodThoughtNotifier(message_file=message_file)
     asyncio.run(gtn.run())
